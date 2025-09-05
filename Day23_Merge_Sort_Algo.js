730e3593e43ba9688f12a39605cb33b9cdd8723a
Ritesh Narwade <riteshnarwade03@gmail.com>
2025-09-05 22:29:09 +0530

    Learning the Merge Sort Algo

---
+++ Day23_Merge_Sort_Algo.js
@@ -0,0 +1,8 @@
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+
+  let mid = arr.length / 2;
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return mergeSort(left, right);
+}
