df49c4c3dd6452d9ed1f738565ffbd244980768e
Ritesh Narwade <riteshnarwade03@gmail.com>
2025-09-24 21:54:41 +0530

    Approach 2 with floyd's algorithm

--- Day26_Linked_List_Cycle.js
+++ Day26_Linked_List_Cycle.js
@@ -10,15 +10,29 @@
  * @param {ListNode} head
  * @return {boolean}
  */
+// var hasCycle = function (head) {
+//   let seenNodes = new Set();
+//   let curr = head;
+//   while (curr != null) {
+//     if (seenNodes.has(curr)) {
+//       return true;
+//     }
+//     seenNodes.add(curr);
+//     curr = curr.next;
+//   }
+//   return false;
+// };
+
 var hasCycle = function (head) {
-  let seenNodes = new Set();
-  let curr = head;
-  while (curr != null) {
-    if (seenNodes.has(curr)) {
-      return true;
-    }
-    seenNodes.add(curr);
-    curr = curr.next;
+  if (head == null) false;
+
+  let slow = head;
+  let fast = head.fast;
+
+  while (slow != fast) {
+    if (fast == null || fast.next == null) false;
+    slow = slow.next;
+    fast = fast.next.next;
   }
-  return false;
+  return true;
 };
